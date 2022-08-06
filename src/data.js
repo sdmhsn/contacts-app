4c62fc011bf50ff2d3ab6320ffeb0a0a61410750
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-06 17:07:58 +0800

    Create data module

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: 'Dimas Saputra',
+      tags: 'dimasmds',
+      imageUrl: '/images/dimasmds.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Arif Faizin',
+      tags: 'arifaizin',
+      imageUrl: '/images/arifaizin.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Rahmat Fajri',
+      tags: 'rfajri27',
+      imageUrl: '/images/rfajri27.jpeg',
+    },
+  ];
+};
+
+export { getData };
