f81c950da164526bf9f0b39daa5b65cab6666403
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-06 18:20:10 +0800

    Repaired "tags" property typo in ContactItem module

--- src/ContactItem.js
+++ src/ContactItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tags }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
-      <ContactItemBody name={name} tag={tag} />
+      <ContactItemBody name={name} tag={tags} />
     </div>
   );
 }
