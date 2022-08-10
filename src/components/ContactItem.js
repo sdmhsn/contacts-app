8d4fcaf3665488b5ae6b197b43381f2f2237bb5b
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-10 17:21:35 +0800

    Added DeleteButton components, id and onDelete properties, into ContactItem module

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
 
-function ContactItem({ imageUrl, name, tags }) {
+function ContactItem({ imageUrl, name, tags, id, onDelete }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tags} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   );
 }
