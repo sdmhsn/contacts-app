9096315945f9982ea528681440faa365f58f97db
Saddam Husein <saddambinhusein2@gmail.com>
2022-08-12 10:58:04 +0800

    Modified ContactApp module: Added ContactInput component  with onAddContactHandler

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
 
 class ContactApp extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       contacts: getData(),
     };
+
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
     this.onDeleteEventHandler = this.onDeleteEventHandler.bind(this);
+  }
+
+  onAddContactHandler({ name, tags }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tags,
+            imageUrl: '/images/default.jpg',
+          },
+        ],
+      };
+    });
   }
 
   onDeleteEventHandler(id) {
@@ -24,7 +44,10 @@ class ContactApp extends React.Component {
   render() {
     return (
       <div className="contact-app">
-        <h1>Contact List</h1>
+        <h1>Contact App</h1>
+        <h2>Add Contact</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <h2>Contact List</h2>
         <ContactList
           contacts={this.state.contacts}
           onDelete={this.onDeleteEventHandler}
